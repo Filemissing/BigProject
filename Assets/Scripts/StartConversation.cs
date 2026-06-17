3c835b3bd20ae0f6496c0325fd640820e4d7b58e
MiraK5 <samijkostova@gmail.com>
2026-06-17 14:21:48 +0200

    Fixed ?

--- Assets/Scripts/StartConversation.cs
+++ Assets/Scripts/StartConversation.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 public class StartConversation : MonoBehaviour
 {
     [SerializeField] UnityEvent unityEvent;
+    [SerializeField] float delay = 1;
 
     void Start()
     {
-        unityEvent.Invoke();
+        StartCoroutine(DelayedStart());
+
+        IEnumerator DelayedStart()
+        {
+            yield return new WaitForSeconds(delay);
+            unityEvent.Invoke();
+        }
     }
 }
